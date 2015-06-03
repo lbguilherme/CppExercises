8e2ad2952d938eb40173c6a51d070aafc372f3a4
Guilherme Bernal <dev@lbguilherme.com>
2015-06-03 15:02:26 -0300

    Add timeout assertion

--- internal.hpp
+++ internal.hpp
@@ -5,5 +5,36 @@
 #endif
 
 #include <iostream>
-#define ASSERT(x) if (!(x)) {std::cout << "Failed '" __FILE__ "': at line " << __LINE__ << ": assert failed: " #x << std::endl; std::exit(1);}
-#define MAIN(tests) int main() {tests std::cout << "Passed '" __FILE__ "'." << std::endl; return 0;}
+#include <thread>
+using namespace std::literals;
+
+#define ASSERT(x) \
+	if (!(x)) { \
+		std::cout << "Failed '" __FILE__ "': at line " << __LINE__ << ": assert failed: " #x << std::endl; \
+		std::exit(1); \
+	}
+
+#define TIMEOUT(time, code) \
+	{ \
+		bool* done = new bool; \
+		*done = false; \
+		std::thread t([=]{ \
+			std::this_thread::sleep_for(time); \
+			if (!*done) { \
+				std::cout << "Failed '" __FILE__ "': at line " << __LINE__ << ": timed out. "; \
+				std::cout << "You had only " << #time << "." << std::endl; \
+				std::exit(1); \
+			} \
+			delete done; \
+		}); \
+		{code} \
+		*done = true; \
+		t.detach(); \
+	}
+
+#define MAIN(tests) \
+	int main() { \
+		{tests} \
+		std::cout << "Passed '" __FILE__ "'." << std::endl; \
+		return 0; \
+	}
